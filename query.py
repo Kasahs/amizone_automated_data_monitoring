f42d66fb9eb0842567a09ff4d320ef4ddf73a8c4
Manik Raina <manikraina1096@gmail.com>
2020-03-22 19:53:16 +0530

    scaffolding

--- query.py
+++
@@ -1,8 +0,0 @@
-import db
-mydb=db.establish_con("localhost", "manik", "sweetbread", "amizone")
-query = "SELECT class_time, class_loc, course_name FROM homepage_tt;"
-mycursor=db.run_sql(mydb, query)
-for (class_time, class_loc, course_name) in  mycursor:
-    print("{} is at {} in {}".format(course_name, class_time, class_loc))
-mydb.commit()
-
