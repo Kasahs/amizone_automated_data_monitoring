8dc61efc7507aba0d40d0fdee78df61686d74072
Shasak Raina <shasak.sync@gmail.com>
2020-03-29 16:34:33 +0530

    added timtable rendering logic

--- api/main.js
+++ api/main.js
@@ -1,96 +1,84 @@
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
+const express = require("express")
+const app = express()
+const mysql = require("mysql")
 const cors = require("cors")
 
 // TODO: make proper useful select queries
-// TODO: tt should show free slots 
+// TODO: tt should show free slots
 // TODO: tt text should not wrap
 // TODO: full tt from mon-sun should be shown
 // TODO: notification of next class should be given
 // TODO: notification of attendace marked should be given
 // TODO: status of atttendance should be shown
 
-let time_slot_dict =
-{
-  nine_to_ten: '09:15 - 10:10',
-  ten_to_eleven: '10:15 - 11:10',
-  eleven_to_twelve: '11:15 - 12:10',
-  twelve_to_thirteen: '12:15 - 13:10',
-  thiteen_to_fourteen: '13:15 - 14:10',
-  fourteen_to_fifteen: '14:15 - 15:10',
-  fifteen_to_sixteen: '15:15 - 16:10',
-  sixteen_to_seventeen: '16:15 - 17:10',
-  seventeen_to_eighteen: '17:10 - 18:10'
+let time_slot_dict = {
+  nine_to_ten: "09:15 - 10:10",
+  ten_to_eleven: "10:15 - 11:10",
+  eleven_to_twelve: "11:15 - 12:10",
+  twelve_to_thirteen: "12:15 - 13:10",
+  thiteen_to_fourteen: "13:15 - 14:10",
+  fourteen_to_fifteen: "14:15 - 15:10",
+  fifteen_to_sixteen: "15:15 - 16:10",
+  sixteen_to_seventeen: "16:15 - 17:10",
+  seventeen_to_eighteen: "17:10 - 18:10"
 }
-
 
 const createConnection = () => {
   var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'manik',
-    password: 'sweetbread',
-    database: 'amizone'
-  });
+    host: "192.168.1.107",
+    port: "",
+    user: "manik",
+    password: "sweetbread",
+    database: "amizone"
+  })
 
   return connection
-
 }
 
-
-const fetchTimeTable = (connection) => {
-
-  whereQuery = (weekdayNumber) => {
-    query = `WHERE WEEKDAY(\`date\`) = ${weekdayNumber};`
-    return query
-  }
-  main_query = 'SELECT class_time, course_name, class_loc, `date` FROM homepage_tt '
-  
+const fetchTimeTable = connection => {
+  const query =
+    "SELECT class_time, course_name, class_loc, `date` FROM homepage_tt;"
   const promise = new Promise((resolve, reject) => {
-    connection.connect();
-        query = main_query + whereQuery(i)
-      connection.query(query, function (error, results, fields) {
-        if (error) { return reject(err) }
-        // for(const result of results){
-        //   console.log(result.course_name)
-        // }
-        const refinedResults = results.map((result) => {
-          return {
-            courseName: result.course_name,
-            courseTime: result.class_time,
-            classLoc: result.class_loc,
-            classDate: result.date
-          }
-        });
-        refinedResults.forEach(element => {
-          if (element.class_time == time_slot_dict.nine_to_ten) {
-            period = element.class_name;
-          }
-        });
-        connection.end();
-        return resolve(refinedResults)
-      })
-    
-    })
-  return promise
+    connection.connect()
+    connection.query(query, function(error, results, fields) {
+      if (error) {
+        return reject(err)
+      }
 
+      const refinedResults = results.map(result => {
+        return {
+          courseName: result.course_name,
+          courseTime: result.class_time,
+          classLoc: result.class_loc,
+          classDate: result.date
+        }
+      })
+      refinedResults.forEach(element => {
+        if (element.class_time == time_slot_dict.nine_to_ten) {
+          period = element.class_name
+        }
+      })
+      connection.end()
+      return resolve(refinedResults)
+    })
+  })
+  return promise
 }
 
 app.use(cors())
 
-app.get('/', (req, res) => {
-  res.send(("<h1>hello world</h1>"));
-});
+app.get("/", (req, res) => {
+  res.send("<h1>hello world</h1>")
+})
 
-app.get('/api/timetable', async (req, res) => {
+app.get("/api/timetable", async (req, res) => {
   // TODO: fetch timetable from db
   // TODO: compose into proper response JSON object
   // TODO: respond with json object
   const connection = createConnection()
   const results = await fetchTimeTable(connection)
   res.json(results)
-
-});
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`listening on port ${port}`))
--- client/index.js
+++ client/index.js
@@ -1,46 +1,80 @@
-window.addEventListener('load', async () => {
-    await main()
+window.addEventListener("load", async () => {
+  await main()
 })
 
 const main = async () => {
-    // put all stuff here
+  // put all stuff here
 
-    const el = document.getElementById("el")
+  const el = document.getElementById("el")
 
-    const res = await fetch("http://localhost:3000/api/timetable")
-    const data = await res.json()
-    console.dir(data)
+  const res = await fetch("http://localhost:3000/api/timetable")
+  const data = await res.json()
+  console.dir(data)
 
-    el.innerHTML = data.map(d => `<li>${d.courseName}</li>`).join(",<br>")
-}
+  //   el.innerHTML = data.map(d => `<li>${d.courseName}</li>`).join(",<br>")
+  const timeTable = {
+    monday: {
+      slot1: { courseName: "math" },
+      slot3: { courseName: "science" }
+    },
 
+    tuesday: {
+      slot2: { courseName: "math" },
+      slot5: { courseName: "science" }
+    }
+  }
 
-const slots = [
+  const slots = [
     { name: "slot1", startTime: "", endTime: "" },
     { name: "slot2", startTime: "", endTime: "" },
     { name: "slot3", startTime: "", endTime: "" },
     { name: "slot4", startTime: "", endTime: "" },
-    { name: "slot5", startTime: "", endTime: "" },
-]
+    { name: "slot5", startTime: "", endTime: "" }
+  ]
 
-const days = ["monday", "tuesday", "wednesday"]
+  const days = ["monday", "tuesday", "wednesday"]
 
-const timetable = {
-    "monday": {
-        "slot1": { courseName: "math" },
-        "slot3": { courseName: "science" }
-    },
-
-
-    "tuesday": {
-        "slot2": { courseName: "math" },
-        "slot5": { courseName: "science" }
-    },
+  renderTimeTable(el, timeTable, days, slots)
 }
 
-const renderTimeTable = (timetable, days, slots) => {
+const arrayToRowItems = items => {
+  return `${items.map(item => `<td>${item}</td>`).join("\n")}`
+}
 
-    days.forEach(day => {
+const arrayToTableRow = items => {
+  return `<tr>${arrayToRowItems(items)}</tr>`
+}
+const arrayToTableHead = items => {
+  return `<thead>${arrayToRowItems(items)}</thead>`
+}
 
-    });
+const timetableRows = (timetable, days, slots) => {
+  const rows = []
+  days.forEach(day => {
+    const row = [day]
+    slots.forEach(slot => {
+      row.push(timetable[day][slot] || "")
+    })
+    rows.push(row)
+  })
+  return rows
+}
+
+const timeTableToHtml = (timetable, days, slots) => {
+  const dataInRows = timetableRows(timetable, days, slots)
+  const tableContentHtml = ""
+  tableContentHtml += arrayToTableHead(["day/time", ...slots])
+
+  const rowsHtml = dataInRows.map(row => arrayToTableRow).join("\n")
+  tableContentHtml += rowsHtml
+  return `<table>${tableContentHtml}</table>`
+}
+
+const renderHtml = (el, html) => {
+  el.innerHTML = html
+  return el
+}
+
+const renderTimeTable = (el, timetable, days, slots) => {
+  return renderHtml(el, timeTableToHtml(timetable, days, slots))
 }
