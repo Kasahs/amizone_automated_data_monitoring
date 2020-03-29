1b8bc868db0b7ee670a0cdfbb24e48f71a747616
Shasak Raina <shasak.sync@gmail.com>
2020-03-29 18:10:37 +0530

    made table renderer more general

--- client/index.js
+++ client/index.js
@@ -10,69 +10,81 @@ const main = async () => {
   //   console.dir(data)
 
   //   el.innerHTML = data.map(d => `<li>${d.courseName}</li>`).join(",<br>")
-  const timeTable = {
+
+  const data = {
     monday: {
-      slot1: { courseName: "math" },
-      slot3: { courseName: "science" }
+      "1": "math",
+      "3": "science"
     },
 
     tuesday: {
-      slot2: { courseName: "math" },
-      slot5: { courseName: "science" }
+      "2": "english",
+      "5": "history"
     }
   }
 
-  const slots = [
-    { name: "slot1", startTime: "", endTime: "" },
-    { name: "slot2", startTime: "", endTime: "" },
-    { name: "slot3", startTime: "", endTime: "" },
-    { name: "slot4", startTime: "", endTime: "" },
-    { name: "slot5", startTime: "", endTime: "" }
+  const slots = ["1", "2", "3", "4", "5"]
+
+  const days = [
+    "monday",
+    "tuesday",
+    "wednesday",
+    "thursday",
+    "friday",
+    "saturday",
+    "sunday"
   ]
-
-  const days = ["monday", "tuesday", "wednesday"]
   const el = document.getElementById("el")
-  renderTimeTable(el, timeTable, days, slots)
+  const renderTable = Table(days, slots, "day/slot")
+  renderTable(el, data)
 }
 
-const arrayToRowItems = items => {
-  return `${items.map(item => `<td>${item}</td>`).join("\n")}`
-}
+const Table = (vLabels, hLabels, pivot) => {
+  const _arrayToRowItems = items => {
+    const html = `${items.map(item => `<td>${item}</td>`).join("\n")}`
+    return html
+  }
 
-const arrayToTableRow = items => {
-  return `<tr>${arrayToRowItems(items)}</tr>`
-}
-const arrayToTableHead = items => {
-  return `<thead>${arrayToRowItems(items)}</thead>`
-}
+  const _arrayToTableRow = items => {
+    return `<tr>${_arrayToRowItems(items)}</tr>`
+  }
 
-const timetableRows = (timetable, days, slots) => {
-  const rows = []
-  days.forEach(day => {
-    const row = [day]
-    slots.forEach(slot => {
-      row.push(timetable[day][slot] || "")
+  const _arrayToTableHead = items => {
+    return `<thead>${_arrayToRowItems(items)}</thead>`
+  }
+
+  const _dataAsRows = (data, vLabels, hLabels) => {
+    const rows = []
+    vLabels.forEach(vLabel => {
+      const row = [vLabel]
+      hLabels.forEach(hLabel => {
+        row.push(
+          data[vLabel] ? (data[vLabel][hLabel] ? data[vLabel][hLabel] : "") : ""
+        )
+      })
+      rows.push(row)
     })
-    rows.push(row)
-  })
-  return rows
-}
+    return rows
+  }
 
-const timeTableToHtml = (timetable, days, slots) => {
-  const dataInRows = timetableRows(timetable, days, slots)
-  const tableContentHtml = ""
-  tableContentHtml += arrayToTableHead(["day/time", ...slots])
+  const _dataToHtmlTable = (data, vLabels, hLabels, pivot) => {
+    const dataInRows = _dataAsRows(data, vLabels, hLabels)
+    let tableContentHtml = ""
+    tableContentHtml += _arrayToTableHead([pivot, ...hLabels])
 
-  const rowsHtml = dataInRows.map(row => arrayToTableRow).join("\n")
-  tableContentHtml += rowsHtml
-  return `<table>${tableContentHtml}</table>`
-}
+    const rowsHtml = dataInRows.map(row => _arrayToTableRow(row)).join("\n")
+    tableContentHtml += rowsHtml
+    return `<table>${tableContentHtml}</table>`
+  }
 
-const renderHtml = (el, html) => {
-  el.innerHTML = html
-  return el
-}
+  const _renderHtml = (el, html) => {
+    el.innerHTML = html
+    return el
+  }
 
-const renderTimeTable = (el, timetable, days, slots) => {
-  return renderHtml(el, timeTableToHtml(timetable, days, slots))
+  const renderTable = (el, data) => {
+    return _renderHtml(el, _dataToHtmlTable(data, vLabels, hLabels, pivot))
+  }
+
+  return renderTable
 }
